9c0be1fd95fd3a3d8e91e3ff6d6c62ddf757d598
snowsolstice <127462560+snowsolstice@users.noreply.github.com>
2023-03-09 23:19:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title("my parents healthy diner 🥣 🥗 🐔 🥑🍞")
 
-streamlit.dataframe(my_fruit_list)
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avacado", "Strawberries"])
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+streamlit.dataframe(fruits_to_show)
