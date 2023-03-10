4eeaa3ca7589e98ca367156bf7e25f1d78b473d1
snowsolstice <127462560+snowsolstice@users.noreply.github.com>
2023-03-09 23:04:49 -0500

    Update streamlit_app.py
    import pandas, create dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit
+import pandas as pd
+
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
 streamlit.title("my parents healthy diner 🥣 🥗 🐔 🥑🍞")
 
 streamlit.header("I resent you for this")
 
 streamlit.text("this is just html")
+
+streamlit.dataframe(My_fruit_list)
