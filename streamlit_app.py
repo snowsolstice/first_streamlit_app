9923ca8f087139b04b588d57015c088bfafff792
snowsolstice <127462560+snowsolstice@users.noreply.github.com>
2023-03-09 21:51:34 -0500

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title("my parents healthy diner")
 
-streaamlit.header("I resent you for this")
+streamlit.header("I resent you for this")
 
 streamlit.text("this is just html")
