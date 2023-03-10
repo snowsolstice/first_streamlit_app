a30f4449a6752585e96b59efc7a1fe1f2dacd6d9
snowsolstice <127462560+snowsolstice@users.noreply.github.com>
2023-03-09 23:16:25 -0500

    Update streamlit_app.py
    added index and data selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title("my parents healthy diner 🥣 🥗 🐔 🥑🍞")
 
-streamlit.header("I resent you for this")
-
-streamlit.text("this is just html")
-
 streamlit.dataframe(my_fruit_list)
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
